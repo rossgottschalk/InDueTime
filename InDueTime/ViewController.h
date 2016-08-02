3a4bb5038bfcb59e76f98f2c0a2a8e1b9c8a6687
Ross Gottschalk <rossgottschalk@yahoo.com>
2016-08-02 15:44:06 -0400

    Initial Commit

---
+++ InDueTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InDueTime
+//
+//  Created by Ross Gottschalk on 8/2/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InDueTime/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InDueTime
+//
+//  Created by Ross Gottschalk on 8/2/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
